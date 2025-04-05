0bd7bb0163b0120690943d6b2cb9f2c83e794160
maihuyen137 <maihuyen1372k2@gmail.com>
2025-04-05 17:12:57 +0700

    Update pratice_1.sql

--- pratice_1.sql
+++ pratice_1.sql
@@ -1,1 +1,13 @@
-
+-- Ex9: recyclable and low-fat products
+SELECT product_id FROM Products
+WHERE low_fats = 'Y'
+AND recyclable = 'Y'
+-- Ex10: find customer referee
+SELECT name FROM Customer
+WHERE NOT referee_id = 2
+OR referee_id IS NULL
+-- Ex11: big countries
+SELECT name, population, area FROM World
+WHERE area >= 3000000
+OR population >= 25000000
+-- Ex12: 
