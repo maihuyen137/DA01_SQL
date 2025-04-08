01fb38f32660a83577e9dadd3b08013fccc8ca19
maihuyen137 <maihuyen1372k2@gmail.com>
2025-04-08 10:57:32 +0700

    practice_2.sql

--- practice_2.sql
+++ practice_2.sql
@@ -1,4 +1,25 @@
---- Ex2:
+-- Ex2:
 SELECT
 COUNT(*) - COUNT(DISTINCT(CITY))
 FROM STATION;
+
+-- Ex4:
+SELECT
+ROUND (SUM(item_count*order_occurrences)::NUMERIC/SUM(order_occurrences),1)
+FROM items_per_order
+
+-- Ex5:
+SELECT candidate_id
+FROM candidates
+WHERE skill IN ('Python','Tableau','PostgreSQL')
+GROUP BY candidate_id
+HAVING COUNT(skill IN ('Python','Tableau','PostgreSQL')) =3
+ORDER BY candidate_id
+
+-- Ex7:
+SELECT card_name,
+MAX(issued_amount) - MIN(issued_amount) AS difference
+FROM monthly_cards_issued
+GROUP BY card_name
+ORDER BY difference DESC
+
