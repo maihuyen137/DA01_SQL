5e11e9bf993d3469532999ebc70c81a7804773ea
maihuyen137 <maihuyen1372k2@gmail.com>
2025-04-14 15:42:46 +0700

    Update practice_5.sql

--- practice_5.sql
+++ practice_5.sql
@@ -7,4 +7,30 @@ ON a.CODE=b.COUNTRYCODE
 GROUP BY a.CONTINENT;
 
 -- Ex2:
+SELECT
+ROUND(SUM(CASE WHEN a.signup_action='Confirmed' THEN 1 ELSE 0 END)/COUNT (*)::DECIMAL,2)
+FROM texts AS a
+LEFT JOIN emails AS b
+ON a.email_id=b.email_id;
+
+-- Ex3:
+SELECT b.age_bucket,
+ROUND(100.00*SUM(CASE WHEN a.activity_type='open' THEN a.time_spent ELSE 0 END)/SUM(a.time_spent),2) AS open_perc,
+ROUND(100.00*SUM(CASE WHEN a.activity_type='send' THEN a.time_spent ELSE 0 END)/SUM(a.time_spent),2) AS send_perc
+FROM activities AS a
+LEFT JOIN age_breakdown AS b
+ON a.user_id=b.user_id
+WHERE a.activity_type IN ('open','send')
+GROUP BY b.age_bucket;
+
+-- Ex4:
+SELECT a.customer_id
+FROM customer_contracts AS a
+LEFT JOIN products AS b
+ON a.product_id=b.product_id
+GROUP BY a.customer_id
+HAVING COUNT(DISTINCT b.product_category)=(SELECT COUNT(DISTINCT product_category) FROM products);
+
+-- Ex5:
+-- Ex7:
 
