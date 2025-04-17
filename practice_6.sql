80515ee1ff937998b0388242a2c301c0981da6db
maihuyen137 <maihuyen1372k2@gmail.com>
2025-04-17 16:58:59 +0700

    practice_6.sql

--- practice_6.sql
+++ practice_6.sql
@@ -8,6 +8,20 @@ SELECT COUNT (DISTINCT company_id) AS duplicate_companies
 FROM job_count
 WHERE job_count>1
 
+-- Ex2:
+WITH total_spend AS
+(SELECT category, product,
+SUM(spend) AS total_spend,
+RANK() OVER
+(PARTITION BY category
+ORDER BY SUM(spend) DESC) AS ranking
+FROM product_spend
+WHERE EXTRACT(YEAR FROM transaction_date)='2022'
+GROUP BY category, product)
+SELECT category, product, total_spend
+FROM total_spend
+WHERE ranking<=2
+
 -- Ex3:
 WITH call_count AS
 (SELECT policy_holder_id,
