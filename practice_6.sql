3fbf085b0cd384cafa0b49c5e5f26392dd5a2c4e
maihuyen137 <maihuyen1372k2@gmail.com>
2025-04-18 00:43:01 +0700

    Update practice_6.sql

--- practice_6.sql
+++ practice_6.sql
@@ -49,7 +49,11 @@ EXTRACT(MONTH FROM event_date) AS mth,
 COUNT(DISTINCT user_id) AS monthly_active_users
 FROM user_actions
 WHERE EXTRACT(MONTH FROM event_date)=7
+AND EXTRACT(YEAR FROM event_date)=2022
+AND user_id IN (SELECT user_id FROM active_users_last_mth)
+GROUP BY EXTRACT(MONTH FROM event_date);
 
+-- Ex6:
 SELECT 
 TO_CHAR(trans_date,'YYYY-MM') AS month,
 country,
@@ -58,8 +62,6 @@ SUM(CASE WHEN state='approved' THEN 1 ELSE 0 END) AS approved_count,
 SUM (amount) AS trans_total_amount,
 SUM(CASE WHEN state='approved' THEN amount ELSE 0 END) AS approved_total_amout
 FROM Transactions
-GROUP BY country,TO_CHAR(trans_date,'YYYY-MM')
-AND EXTRACT(YEAR FROM event_date)=2022
-AND user_id IN (SELECT user_id FROM active_users_last_mth)
-GROUP BY EXTRACT(MONTH FROM event_date);
+GROUP BY TO_CHAR(trans_date,'YYYY-MM'),country;
+
 
