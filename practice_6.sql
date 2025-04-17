976bf8a9dd2d62324dada81698139e80ec54b6bf
maihuyen137 <maihuyen1372k2@gmail.com>
2025-04-17 15:29:01 +0700

    Update practice_6.sql

--- practice_6.sql
+++ practice_6.sql
@@ -7,3 +7,14 @@ GROUP BY company_id, title, description)
 SELECT COUNT (DISTINCT company_id) AS duplicate_companies
 FROM job_count
 WHERE job_count>1
+
+-- Ex3:
+WITH call_count AS
+(SELECT policy_holder_id,
+COUNT(case_id) AS call_count
+FROM callers
+GROUP BY policy_holder_id
+HAVING COUNT(case_id)>=3)
+SELECT
+COUNT(policy_holder_id) AS policy_holder_count
+FROM call_count
