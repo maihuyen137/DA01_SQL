fd4d71e1618b89e016a89b2dca2998bfbaeab9ea
maihuyen137 <maihuyen1372k2@gmail.com>
2025-04-18 16:17:28 +0700

    Update practice_6.sql

--- practice_6.sql
+++ practice_6.sql
@@ -60,8 +60,59 @@ country,
 COUNT(*) AS trans_count,
 SUM(CASE WHEN state='approved' THEN 1 ELSE 0 END) AS approved_count,
 SUM (amount) AS trans_total_amount,
-SUM(CASE WHEN state='approved' THEN amount ELSE 0 END) AS approved_total_amout
+SUM(CASE WHEN state='approved' THEN amount ELSE 0 END) AS approved_total_amount
 FROM Transactions
 GROUP BY TO_CHAR(trans_date,'YYYY-MM'),country;
 
+-- Ex7:
+SELECT product_id, year AS first_year, quantity, price
+FROM Sales
+WHERE (product_id,year) IN
+(SELECT product_id, MIN (year)
+FROM Sales
+GROUP BY product_id);
 
+-- Ex8:
+SELECT customer_id 
+FROM Customer
+GROUP BY customer_id
+HAVING COUNT(DISTINCT product_key) = (SELECT COUNT(product_key) FROM Product);
+mmnb
+-- Ex9:
+SELECT employee_id
+FROM Employees
+WHERE salary < 30000
+AND manager_id NOT IN (SELECT employee_id FROM Employees)
+
+-- Ex10: Bài này giống bài 1 ạ
+-- Ex11:
+WITH name AS
+(SELECT b.name
+FROM MovieRating AS a
+JOIN Users AS b ON a.user_id=b.user_id
+GROUP BY a.user_id, b.name
+ORDER BY COUNT(*) DESC, b.name
+LIMIT 1),
+highest_avg_rating AS
+(SELECT d.title
+FROM MovieRating AS c
+JOIN Movies AS d ON c.movie_id=d.movie_id
+WHERE created_at BETWEEN '2020-02-01' AND '2020-02-28'
+GROUP BY c.movie_id, d.title
+ORDER BY AVG(c.rating) DESC, d.title
+LIMIT 1)
+SELECT name AS results FROM name
+UNION ALL
+SELECT title FROM highest_avg_rating
+
+-- Ex12:
+WITH ids AS
+(SELECT requester_id AS id FROM RequestAccepted
+UNION ALL
+SELECT accepter_id AS id FROM RequestAccepted)
+SELECT id,
+COUNT(*) AS num
+FROM ids
+GROUP BY id
+ORDER BY num DESC
+LIMIT 1;
