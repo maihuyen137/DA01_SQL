2cacd8e085c8ebcb46187a9025fbdb10a2981ed4
maihuyen137 <maihuyen1372k2@gmail.com>
2025-04-20 16:45:48 +0700

    Update practice_7.sql

--- practice_7.sql
+++ practice_7.sql
@@ -1,3 +1,21 @@
+-- Ex1:
+WITH yearly_spend AS
+(SELECT
+EXTRACT(year FROM transaction_date) AS year,
+product_id,
+spend AS curr_year_spend,
+LAG(spend) OVER(PARTITION BY product_id ORDER BY transaction_date) AS prev_year_spend
+FROM user_transactions)
+SELECT *,
+ROUND(100*(curr_year_spend-prev_year_spend)/prev_year_spend,2) AS yoy_rate
+FROM yearly_spend;
+
+-- Ex2:
+SELECT DISTINCT card_name,
+FIRST_VALUE(issued_amount) OVER(PARTITION BY card_name ORDER BY issue_year, issue_month) AS issued_amount
+FROM monthly_cards_issued
+ORDER BY issued_amount DESC;
+
 -- Ex8:
 WITH ranking AS
 (SELECT a.artist_name,
